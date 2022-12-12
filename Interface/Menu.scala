fa7997903e960de836e5d9c6ff3572f48ed2e11c
Lucas Holanda <114670798+LuikHol@users.noreply.github.com>
2022-12-12 20:35:42 -0300

    Update Menu.scala

--- Interface/Menu.scala
+++ Interface/Menu.scala
@@ -1,6 +1,7 @@
 package Interface
 import scala.io.StdIn.*
 import Operacoes.*
+import scala.collection.mutable.ListBuffer
 
 /*Fazer o Acumulador receber os resultados das operações desejadas
   Criar o histórico de operações
@@ -8,32 +9,71 @@ import Operacoes.*
   Importar os objetos que realizam as operações*/
 
 object Menu extends Exception{
-  var acumulador = 0
-  var flag=true
-  while(flag) {
-    if(acumulador==0) {
-      println("SELECIONE A AÇÃO DESEJADA:")
-      println("1 - Operações básicas")
-      println("2 - Operações Avançadas:")
-      println("3- Histórico de Operações(10 últimas):")
-      println("4 - Limpar entrada")
-      println("5 -Sair")
-    }else{
-      Println("Resultado: " + acumulador)
 
-      println("SELECIONE A AÇÃO DESEJADA:")
-      println("1 - Operações básicas")
-      println("2 - Operações Avançadas:")
-      println("3- Histórico de Operações(10 últimas):")
-      println("4 - Limpar entrada")
-      println("5 -Sair")
+  def main(args: Array[String]): Unit = {
+    var acumulador = 0.0
+    var flag = true
+    var historico = ListBuffer[String]()
+
+
+    while (flag) {
+      if (acumulador == 0) {
+        println("SELECIONE A AÇÃO DESEJADA:")
+        println("1 - Operações básicas (+ - * /)")
+        println("2 - Operações Avançadas")
+        println("3- Histórico de Operações(10 últimas)")
+        println("4 - Limpar entrada")
+        println("5 - Sair")
+        var x = readInt()
+
+        x match {
+          case 1 => val valor = ResolveExpressao
+            println("Digite a expressão: ")
+            val result = readLine()
+            val aux = result
+            acumulador = valor.eval(result)
+            historico += s"$aux = $acumulador"
+          case 3 => val lista = historico.toList.take(10)
+            for (l <- lista)
+              println(l)
+          case 5 => println("Até logo!")
+            flag = false
+          case _ => println("Digite um valor válido!")
+        }
+
+      } else {
+        println("Resultado: " + acumulador)
+
+        println("SELECIONE A AÇÃO DESEJADA:")
+        println("1 - Operações básicas")
+        println("2 - Operações Avançadas:")
+        println("3- Histórico de Operações(10 últimas):")
+        println("4 - Limpar entrada")
+        println("5 -Sair")
+
+        var x = readInt()
+
+        x match {
+          case 1 => val valor = ResolveExpressao
+            println("Digite a expressão: ")
+            val result = readLine()
+            val aux = result
+            acumulador = valor.eval(result)
+            historico += s"$aux = $acumulador"
+          case 3 => val lista = historico.toList.take(10)
+            for (l <- lista)
+              println(l)
+          case 4 => acumulador = 0.0
+          case 5 => println("Até logo!")
+            flag = false
+          case _ => println("Digite um valor válido!")
+        }
+      }
+      try {
+        var x = readInt();
+      } catch {
+        case e => NumberFormatException("INSIRA VALOR VÁLIDO")
+      }
     }
-    try{
-      var x=readInt();
-    }catch {
-      case e=>NumberFormatException("INSIRA VALOR VÁLIDO")
-    }
-
-
   }
 }
