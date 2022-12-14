3b5e7722c35026c9caf2682325a5d76dac502dca
Lucas Holanda <114670798+LuikHol@users.noreply.github.com>
2022-12-13 23:30:12 -0300

    Update Menu.scala

--- src/main/scala/Interface/Menu.scala
+++ src/main/scala/Interface/Menu.scala
@@ -10,7 +10,6 @@ object Menu extends Exception{
     var acumulador = 0.0
     var flag = true
     var historico = ListBuffer[String]()
-    
     /* Listas em Scala são imutáveis, um claro sinal do paradigma funcional
        então para casos onde queremos listas que serão constantemente modificadas
        é comum utilizar o ListBuffer para criar listas modificáveis
@@ -22,15 +21,19 @@ object Menu extends Exception{
         println("1 - Calcular Expressão")
         println("2 - Histórico de Operações(10 últimas)")
         println("3 - Limpar entrada")
-        println("4 - Lista de operações")
-        println("5 - Sair")
+        println("4 - Utilizar PI (π)")
+        println("5 - Utilizar Euler (e)")
+        println("6 - Lista de operações")
+        println("7 - Sair")
       } else {
         println("SELECIONE A AÇÃO DESEJADA:")
         println("1 - Calcular Expressão")
         println("2 - Histórico de Operações(10 últimas):")
         println("3 - Limpar entrada")
-        println("4 - Lista de operações")
-        println("5 - Sair")
+        println("4 - Utilizar PI (π)")
+        println("5 - Utilizar Euler (e)")
+        println("6 - Lista de operações")
+        println("7 - Sair")
         println("Resultado: " + acumulador)
       }
       val input=readLine()
@@ -59,7 +62,9 @@ object Menu extends Exception{
           for (l <- lista)
             println(l)
         case "3" => acumulador = 0.0
-        case "4" =>
+        case "4" => acumulador = 3.14159265359
+        case "5" => acumulador = 2.7182818284590452353602874713527
+        case "6" =>
           println("+ -> Somar | Ex: a+b")
           println("- -> Subtrair | Ex: a-b")
           println("* -> Multiplicar | Ex: a*b")
@@ -67,8 +72,7 @@ object Menu extends Exception{
           println("% -> Modulo | Ex: a%b")
           println("p -> Potenciação | Ex: 2p2 = 4")
           println("r -> Raiz Quadrada | Ex: 2r = 4")
-          println("l - > Logaritmo Natural | Ex: 2l = 0.69...")
-          println("!-> Fatorial | Ex: 5!=120")
+          println("l - > Logaritmo | Ex: 2l = 0.69...")
           println("s-> Seno | Ex: 45s = 0.85...")
           println("c-> Cosseno | Ex: 45c = 0.52...")
           println("t-> Tangente | Ex: 45t = 1.61...")
@@ -76,10 +80,11 @@ object Menu extends Exception{
           println(") -> Fecha Parênteses")
           println("OBS: Ao realizar uma operação lembre-se sempre de colocar o sinal APÓS o número")
 
-        case "5" => println("ENCERRANDO")
+        case "7" => println("ENCERRANDO")
           flag = false
         case _ => println("INSIRA VALOR VÁLIDO")
       }
     }
   }
 }
+
